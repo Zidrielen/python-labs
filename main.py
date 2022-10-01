16ae0eb2132b483718fa3fb315487c4b42a9a0dc
Zaitcev Ilya <dp.io1866@yandex.ru>
2022-10-01 18:10:08 +0400

    Added saving the html-code of the page

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from utils import main
+import utils
+
+def main():
+    link_cat="https://yandex.ru/images/search?text=cat"
+    link_dog="https://yandex.ru/images/search?text=dog"
+
+    utils.make_dir()
+    response = utils.save_html(link_cat)
+
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 from os import mkdir
+import requests
+import re
+from bs4 import BeautifulSoup
 
 def make_dir():
     mkdir("dataset")
     mkdir("dataset\cat")
     mkdir("dataset\dog")
 
-def main():
-    make_dir()
-    
+def save_html(link):
+    return requests.get(link).text
+
