f0987899c4cbb13276f37f90ffcf23ac93d03839
Zaitcev Ilya <dp.io1866@yandex.ru>
2022-10-10 14:16:26 +0400

    It remains to add the function of deleting identical images

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from time import sleep
-from os import mkdir
+from os import mkdir, replace, listdir
+from imghdr import what
 
 import requests
 from bs4 import BeautifulSoup
@@ -45,6 +46,30 @@ def push_image(animal):
         save_image(response, animal, n)
 
 
+def remove_stranger(animal, stranger):
+    mkdir(f"dataset\stranger_{animal}")
+
+    for image in stranger:
+        sourse = f"dataset\{animal}\{image}"
+        destination = f"dataset\stranger_{animal}\{image}"
+        replace(sourse, destination)
+        #Instead of the lines above should be this
+        #remove(f"dataset\dog\{image}")
+
+
+def cheking_format(animal):
+    non_jpg_dir = f"dataset/non_jpg_format_{animal}"
+    mkdir(non_jpg_dir)
+
+    images = listdir(f"dataset\{animal}")
+    for image in images:
+        path = f"dataset\{animal}\{image}"
+        if(what(path) != "jpeg"):
+            replace(path, non_jpg_dir + "/" + image)
+            #Instead of the lines above should be this
+            #remove(path)      
+
+
 def main():
     #Make directories for images
     make_dir()
@@ -54,3 +79,25 @@ def main():
 
     #Add images in "dog" directory
     push_image(DOG)
+
+    #Deleting "foreign" images in the "dog" folder
+    stranger_dog = ["0271.jpg", "0318.jpg", "0338.jpg", "0522.jpg", "0559.jpg",
+                    "0383.jpg", "0420.jpg", "0508.jpg", "0547.jpg", "0561.jpg",
+                    "0129.jpg", "0149.jpg", "0154.jpg", "0167.jpg", "0567.jpg",
+                    "0202.jpg", "0235.jpg", "0262.jpg", "0382.jpg", "0574.jpg",
+                    "0607.jpg", "0624.jpg", "0646.jpg", "0672.jpg", "0698.jpg",
+                    "0709.jpg", "0726.jpg", "0737.jpg", "0786.jpg", "0816.jpg",
+                    "0828.jpg", "0843.jpg", "0855.jpg", "0881.jpg", "0942.jpg",
+                    "1022.jpg", "1029.jpg", "1065.jpg", "1072.jpg", "1082.jpg",
+                    "1089.jpg", "1096.jpg"]
+    remove_stranger(DOG, stranger_dog)
+
+    #Deleting "foreign" images in the "cat" folder
+    stranger_cat = ["0410.jpg", "0772.jpg", "0782.jpg", "0911.jpg", "0977.jpg"]
+    remove_stranger(CAT, stranger_cat)
+
+    #Checking for .jpg format
+    cheking_format(CAT)
+    cheking_format(DOG)
+    #all images are in jpg format
+    
