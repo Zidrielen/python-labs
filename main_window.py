e886e87492183ab4e9c05a8789c46ecc0c7e4210
Zaitcev Ilya <dp.io1866@yandex.ru>
2022-11-17 23:14:12 +0400

    Implemented photo viewing in accordance with the condition of the laboratory

--- main_window.py
+++ main_window.py
@@ -1,52 +1,96 @@
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtWidgets import QMenuBar, QMenu, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QLabel, QPushButton, QWidget
+from PyQt5.QtWidgets import QMenuBar, QMenu, QFileDialog, QMessageBox, QDesktopWidget, QVBoxLayout
+from PyQt5.QtGui import QPixmap, QFont
+
+from Lab_2_5 import Iterator_1
 
 import sys
 import os
-from typing import Optional
 
-class Window(QMainWindow):
+class Window(QWidget):
+
     def __init__(self):
-        super(Window, self).__init__()
 
-        self.setWindowTitle("Лабораторная работа №3")
-        self.setGeometry(150, 200, 1000, 600)
+        super().__init__()
 
-        self.main_text = QtWidgets.QLabel(self)
-        text = "В 'Файл' нажмите 'Открыть', чтобы выбрать папку с исходным датасетом."
-        self.main_text.setText(text)
-        self.main_text.setStyleSheet("font-size: 20px")
-        self.main_text.move(160, 270)
-        self.main_text.adjustSize()
+        self.initUI()
 
-        self.createMenuBar()
-    
-    def createMenuBar(self):
-        self.menu_bar = QMenuBar(self)
-        self.setMenuBar(self.menu_bar)
+    def initUI(self) -> None:
 
-        file_menu = QMenu("&Файл", self)
-        self.menu_bar.addMenu(file_menu)
+        self.resize(1200, 800)
+        self.center()
 
-        file_menu.addAction("Открыть", self.check_dir)
+        text = "Нажмите, чтобы выбрать нужную папку с ИСХОДНЫМ датасетом"
+        self.dir_btn = QPushButton(text, self)
+        self.dir_btn.setStyleSheet("font-size: 20px")
+        self.hbox = QVBoxLayout()
+        self.hbox.addWidget(self.dir_btn)
+        self.setLayout(self.hbox)
 
-    def check_dir(self) -> None:
-        folderpath = QtWidgets.QFileDialog.getExistingDirectory(self)
-        if folderpath == "":
+        self.dir_btn.clicked.connect(self.input_path)
+
+        self.setWindowTitle('Лабораторная работа №3')
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def input_path(self) -> None:
+
+        self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(self)
+        if self.folderpath == "":
             return
 
-        if folderpath[-7:] != "dataset":
+        if self.folderpath[-7:] != "dataset":
             self.exception()
             return
         
-        dir_cont = os.listdir(folderpath)
+        dir_cont = os.listdir(self.folderpath)
         if "cat" not in dir_cont or "dog" not in dir_cont:
             self.exception()
             return
 
+        self.general_menu()
+
+    def general_menu(self) -> None:
+
+        self.dir_btn.deleteLater()
+
+        self.it_cat = Iterator_1(self.folderpath, "cat")
+        self.it_dog = Iterator_1(self.folderpath, "dog")
+
+        self.lbl = QLabel(self)
+        self.next_cat()
+
+        next_cat_btn = QPushButton("Следующий кот")
+        next_dog_btn = QPushButton("Следующая собака")
+        next_cat_btn.setStyleSheet("font-size: 20px")
+        next_dog_btn.setStyleSheet("font-size: 20px")
+
+        self.hbox.addWidget(self.lbl)
+        self.hbox.addWidget(next_cat_btn)
+        self.hbox.addWidget(next_dog_btn)
+        self.setLayout(self.hbox)
+
+        next_cat_btn.clicked.connect(self.next_cat)
+        next_dog_btn.clicked.connect(self.next_dog)
+
+    def next_cat(self) -> None:
+
+        self.lbl.setPixmap(QPixmap(next(self.it_cat)))
+        self.lbl.setAlignment(QtCore.Qt.AlignCenter)
+
+    def next_dog(self) -> None:
+
+        self.lbl.setPixmap(QPixmap(next(self.it_dog)))
+        self.lbl.setAlignment(QtCore.Qt.AlignCenter)
 
     def exception(self) -> None:
+
         error = QMessageBox()
         error.setWindowTitle("Ошибка")
         error.setText("Вы выбрали не исходный датасет.")
@@ -59,6 +103,7 @@ class Window(QMainWindow):
 
 
 def application() -> None:
+
     app = QApplication(sys.argv)
     window = Window()
     window.show()
