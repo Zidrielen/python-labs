d9c7e3debbc6b93ff0a6ecb39bb933fa2961c742
Zaitcev Ilya <dp.io1866@yandex.ru>
2022-11-29 12:38:15 +0400

    Completed steps 9 and 10 of lab #4 and improved part_3() function

--- Lab_4.py
+++ Lab_4.py
@@ -1,8 +1,11 @@
 import os
 from typing import Type
+from random import randint
 
 import pandas as pd
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def append_abs_path(abs_path_to_files: list,
@@ -47,29 +50,19 @@ def part_2(my_series: pd.core.series.Series) -> None:
 
 def part_3(my_series: pd.core.series.Series) -> None:
     '''Creating columns with image options'''
-    cat_dir = os.path.join( "dataset", "cat")
-    dog_dir = os.path.join( "dataset", "dog")
-
-    files_cat = os.listdir(cat_dir)
-    files_dog = os.listdir(dog_dir)
-
-    i = 0
-
-    for file in files_cat:
-        image = cv2.imread(os.path.join(cat_dir, file))
-        data = image.shape
-        my_series.loc[[i], "height"] = data[0]
-        my_series.loc[[i], "width"] = data[1]
-        my_series.loc[[i], "depth"] = data[2]
-        i += 1
+    relpaths = []
+    for i in range(0, my_series["abspath"].count()):
+        abspath = my_series.iloc[i, 1]
+        relpaths.append(abspath[-20:])
     
-    for file in files_dog:
-        image = cv2.imread(os.path.join(dog_dir, file))
+    j = 0
+    for path in relpaths:
+        image = cv2.imread(path)
         data = image.shape
-        my_series.loc[[i], "height"] = data[0]
-        my_series.loc[[i], "width"] = data[1]
-        my_series.loc[[i], "depth"] = data[2]
-        i += 1
+        my_series.loc[[j], "height"] = data[0]
+        my_series.loc[[j], "width"] = data[1]
+        my_series.loc[[j], "depth"] = data[2]
+        j += 1
 
 
 def part_4(my_series: pd.core.series.Series) -> None:
@@ -92,7 +85,8 @@ def part_6(my_series: pd.core.series.Series, label: int,
 
 
 def part_7(my_series: pd.core.series.Series, label: int) -> None:
-    
+    '''Creates a column with the number of pixels 
+       and outputs the minimum, maximum, and median pixels'''
     my_series["pixels"] = (my_series["height"] *
                            my_series["width"]  *
                            my_series["depth"])
@@ -102,15 +96,46 @@ def part_7(my_series: pd.core.series.Series, label: int) -> None:
     print("The average number of pixels is", my_series["pixels"].mean())
 
 
+def part_8(my_series: pd.core.series.Series, label: int) -> np.ndarray:
+    
+    label_series = my_series[my_series["label"] == label]
+    rand_num = randint(0, label_series["abspath"].count())
+    abspath = my_series.iloc[rand_num, 1]
+    relpath = abspath[-20:]
+
+    image = cv2.imread(relpath)
+
+    histr_1 = cv2.calcHist([image], [0], None, [256], [0,256])
+    histr_2 = cv2.calcHist([image], [1], None, [256], [0,256])
+    histr_3 = cv2.calcHist([image], [2], None, [256], [0,256])
+    
+    return histr_1, histr_2, histr_3
+
+
+def part_9(histr_1: np.ndarray, histr_2: np.ndarray,
+           histr_3: np.ndarray) -> np.ndarray:
+    
+    col = ('b','g','r')
+    plt.plot(histr_1, color=col[0])
+    plt.plot(histr_2, color=col[1])
+    plt.plot(histr_3, color=col[2])
+    plt.xlabel("value")
+    plt.ylabel("count")
+    plt.title("histogram")
+    plt.xlim([0,256])
+    plt.show()
+
+
 def main() -> None:
     '''Start'''
     my_series = part_1()
     part_2(my_series)
     part_3(my_series)
-    print(part_4(my_series))
-    series_label = part_5(my_series, 1)
-    print(series_label)
-    print(part_6(my_series, 0, 320, 400))
-    part_7(my_series, 0)
-
-
+    print(my_series)
+    #print(part_4(my_series))
+    #series_label = part_5(my_series, 1)
+    #print(series_label)
+    #print(part_6(my_series, 0, 320, 400))
+    #part_7(my_series, 0)
+    #histr_1, histr_2, histr_3 = part_8(my_series, 0)
+    #part_9(histr_1, histr_2, histr_3)
