3eaa3dcf3eb4a5bae149b64aee10fc6065b44db1
Zaitcev Ilya <dp.io1866@yandex.ru>
2022-12-10 19:12:01 +0400

    Added the ability to upload data from a csv file and upload data there. Some parts of the program have been optimized.

--- Lab_4.py
+++ Lab_4.py
@@ -1,4 +1,3 @@
-import os
 from random import randint
 
 import pandas as pd
@@ -7,61 +6,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def append_abs_path(abs_path_to_files: list,
-                    list_files: list, dir: list) -> None:
-    '''Adding the full path to a file to the list'''
-    for files in list_files:
-        path = os.path.join(dir, files)
-        abs_path_to_files.append(path)
-
-
 def part_1() -> pd.core.series.Series:
     '''Creating a DataFrame with column names'''
-    project_path = os.path.abspath("")
-    cat_dir = os.path.join(project_path, "dataset", "cat")
-    dog_dir = os.path.join(project_path, "dataset", "dog")
-
-    files_cat = os.listdir(cat_dir)
-    files_dog = os.listdir(dog_dir)
-    class_ = list(["cat"] * len(files_cat)) + list(["dog"] * len(files_dog))
-
-    abs_path_to_files = []
-    append_abs_path(abs_path_to_files, files_cat, cat_dir)
-    append_abs_path(abs_path_to_files, files_dog, dog_dir)
+    df_start = pd.read_csv("task_1.csv", sep = ";", encoding="windows-1251", header=None)
 
     return pd.DataFrame({
-        "class": class_,
-        "abspath": abs_path_to_files
+        "class": df_start.iloc[:, 2],
+        "abspath": df_start.iloc[:, 0]
     })
 
 
 def part_2(my_series: pd.core.series.Series) -> None:
     '''Create a column with numeric labels'''
-    i = 0
+    my_series["label"] = 0
 
-    for class_ in my_series["class"]:
-        if class_ == "cat":
-            my_series.loc[[i], ["label"]] = 0
-        else:
-            my_series.loc[[i], ["label"]] = 1
-        i += 1
+    for i, class_ in enumerate(my_series["class"]):
+        if class_ == "dog":
+            my_series.iloc[[i], [2]] = 1
 
 
 def part_3(my_series: pd.core.series.Series) -> None:
     '''Creating columns with image options'''
-    relpaths = []
-    for i in range(0, my_series["abspath"].count()):
-        abspath = my_series.iloc[i, 1]
-        relpaths.append(abspath[-20:])
-    
-    j = 0
-    for path in relpaths:
-        image = cv2.imread(path)
-        data = image.shape
-        my_series.loc[[j], "height"] = data[0]
-        my_series.loc[[j], "width"] = data[1]
-        my_series.loc[[j], "depth"] = data[2]
-        j += 1
+    my_series["height"] = 0
+    my_series["width"] = 0
+    my_series["depth"] = 0
+
+    for i, path in enumerate(my_series["abspath"]):
+        data = cv2.imread(path[-20:]).shape
+        my_series.loc[[i], "height"] = data[0]
+        my_series.loc[[i], "width"] = data[1]
+        my_series.loc[[i], "depth"] = data[2]
 
 
 def part_4(my_series: pd.core.series.Series) -> None:
@@ -90,7 +64,7 @@ def part_7(my_series: pd.core.series.Series, label: int) -> None:
                            my_series["width"]  *
                            my_series["depth"])
 
-    pixel_my_series = part_5(label)
+    pixel_my_series = part_5(my_series, label)
     
     print("The maximum number of pixels is ", pixel_my_series["pixels"].max())
     print("The minimum number of pixels is", pixel_my_series["pixels"].min())
@@ -102,9 +76,8 @@ def part_8(my_series: pd.core.series.Series, label: int) -> np.ndarray:
     label_series = my_series[my_series["label"] == label]
     rand_num = randint(0, label_series["abspath"].count())
     abspath = my_series.iloc[rand_num, 1]
-    relpath = abspath[-20:]
 
-    image = cv2.imread(relpath)
+    image = cv2.imread(abspath[-20:])
 
     histr_1 = cv2.calcHist([image], [0], None, [256], [0,256])
     histr_2 = cv2.calcHist([image], [1], None, [256], [0,256])
@@ -125,17 +98,3 @@ def part_9(histr_1: np.ndarray, histr_2: np.ndarray,
     plt.title("histogram")
     plt.xlim([0,256])
     plt.show()
-
-
-def main() -> None:
-    '''Start'''
-    my_series = part_1()
-    part_2(my_series)
-    part_3(my_series)
-    print(part_4(my_series))
-    series_label = part_5(my_series, 1)
-    print(series_label)
-    print(part_6(my_series, 0, 320, 400))
-    part_7(my_series, 0)
-    histr_1, histr_2, histr_3 = part_8(my_series, 0)
-    part_9(histr_1, histr_2, histr_3)
