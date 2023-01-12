b169517e5542ff78393c7084af0f0f141dd85539
Zaitcev Ilya <dp.io1866@yandex.ru>
2023-01-12 13:20:23 +0400

    The image collection function has been changed so that it now collects full-size images. Added Jupyter notebook

--- web_scraping.py
+++ web_scraping.py
@@ -1,57 +1,54 @@
 from os import mkdir, remove, listdir, rename, path
+from urllib.parse import unquote
 from time import sleep
 from imghdr import what
 
 import requests
 from bs4 import BeautifulSoup
-from cv2 import imread, Mat
+from cv2 import imread
 import np
-
-
-LINK_YANDEX="https://yandex.ru/images/search?text="
-LINK_PART_2="&p="
-DOG="dog"
-CAT="cat"
-
-
-def save_html(link):
-    return requests.get(link).text
-
-
-def load_image(link):
-    return requests.get(link, stream=True).content
-
-
-def save_image(response, animal, n):
-    soup = BeautifulSoup(response, "lxml")
-    all_link_tag_img = soup.find_all("img", class_="serp-item__thumb justifier__thumb")
-    for link_tag_img in all_link_tag_img:
-        sleep(5)
-        image = load_image("http:" + link_tag_img.get("src"))
-        name = str(n).zfill(4)
-        path = f"dataset\{animal}\{name}.jpg"
-        file = open(path, "wb")
-        file.write(image)
-        file.close()
-        n += 1
-        
-
-def push_image(animal):
-    for i in range(0, 40):
-        n = 30 * i
-        sleep(30)
-        response = save_html(LINK_YANDEX + animal + LINK_PART_2 + str(i))
-        save_image(response, animal, n)
+import re
 
 
 def make_dir():
     '''Make directories for images'''
-    if not path.isdir("dataset"):
-        mkdir("dataset")
-    if not path.isdir("dataset\cat"):
-        mkdir("dataset\cat")
-    if not path.isdir("dataset\dog"):
-        mkdir("dataset\dog")
+    if not path.isdir("new_dataset"):
+        mkdir("new_dataset")
+    if not path.isdir("new_dataset/cats"):
+        mkdir("new_dataset/cats")
+    if not path.isdir("new_dataset/dogs"):
+        mkdir("new_dataset/dogs")
+
+
+def load_image(animal):
+    '''Collects full size images from the Internet'''
+    for i in range(2, 5):
+        n = 30 * i
+        sleep(30)
+        link_yandex = f"https://yandex.ru/images/search?text={animal}&p={str(i)}"
+        response = requests.get(link_yandex).text
+
+        img_link = []
+        soup = BeautifulSoup(response, "lxml")
+        invalid_img_link = soup.find_all("a", class_="serp-item__link")
+        for img in invalid_img_link:
+            url = re.split("url=", unquote(img.get("href")))[1]
+            if "jpg" in url:
+                img_link.append(url[:url.rfind("jpg") + 3])
+            else:
+                img_link.append(url[:url.rfind("jpeg") + 4])
+
+        for link in img_link:
+            print(link)
+            try:
+                sleep(5)
+                image = requests.get(link, stream=True).content
+                path = f"new_dataset\{animal}\{str(n).zfill(4)}.jpg"
+                with open(path, "wb") as file:
+                    file.write(image)
+                    n += 1
+            except:
+                continue
 
 
 def cheking_format(path_to_folder: str):
@@ -60,15 +57,6 @@ def cheking_format(path_to_folder: str):
         path_to_image = f"{path_to_folder}/{image}"
         if(what(path_to_image) != "jpeg"):
             remove(path_to_image)
-
-
-def rename_image(path_to_folder: str) -> None:
-    '''Changing the name of images'''
-    images = listdir(path_to_folder)
-    for i, image in enumerate(images):
-        before = f"{path_to_folder}/{image}"
-        after = f"{path_to_folder}/{str(i).zfill(4)}.jpg"
-        rename(before, after)
 
 
 def delete_dublicate(path_to_folder: str) -> None:
@@ -85,7 +73,7 @@ def delete_dublicate(path_to_folder: str) -> None:
         j = i + 1
 
         while j <= num_images:
-            if np.all(compare_images(img_data[i], img_data[j])):
+            if np.all(img_data[i] == img_data[j]):
                 remove(f"{path_to_folder}/{image[j]}")
                 print(image[i], "+", image[j])
                 index_array.append(j)
@@ -99,24 +87,10 @@ def delete_dublicate(path_to_folder: str) -> None:
         i += 1
 
 
-def compare_images(img_1: Mat, img_2: Mat) -> bool:
-    return img_1 == img_2
-
-
-def main():
-    PATH_TO_DOG = "dataset/dog"
-    PATH_TO_CAT = "dataset/cat"
-    
-    make_dir()
-
-    push_image(CAT)
-    push_image(DOG)
-
-    cheking_format(PATH_TO_CAT)
-    cheking_format(PATH_TO_DOG)
-
-    delete_dublicate(PATH_TO_CAT)
-    delete_dublicate(PATH_TO_DOG)
-
-    rename_image(PATH_TO_CAT)
-    rename_image(PATH_TO_DOG)
+def rename_image(path_to_folder: str) -> None:
+    '''Changing the name of images'''
+    images = listdir(path_to_folder)
+    for i, image in enumerate(images):
+        before = f"{path_to_folder}/{image}"
+        after = f"{path_to_folder}/{str(i).zfill(4)}.jpg"
+        rename(before, after)
